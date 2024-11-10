016183a9b265f094c6192360a502c270cf186cf6
Raphael <raphaelgoettzinger@gmail.com>
2024-11-10 20:49:59 -0300

    Primeira commit

---
+++ Sentinel/src/main/java/com/raphaelprojetos/Sentinel/SentinelApplication.java
@@ -0,0 +1,18 @@
+package com.raphaelprojetos.Sentinel;
+
+import com.raphaelprojetos.Sentinel.tray.TrayManager;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelApplication {
+
+	public static void main(String[] args) {
+
+		TrayManager trayManager = new TrayManager();
+
+
+	}
+}
+
+
+
---
+++ Sentinel/src/main/java/com/raphaelprojetos/Sentinel/alerts/AlertManager.java
@@ -0,0 +1,5 @@
+package com.raphaelprojetos.Sentinel.alerts;
+
+public class AlertManager {
+
+}
